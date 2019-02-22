7a64cfd215a9d15e64bd3a74c5de39a0ad61be77
Marius Appel <marius.appel@uni-muenster.de>
2019-02-22 13:57:46 +0100

    R CMD check

--- R/plot.R
+++ R/plot.R
@@ -14,7 +14,7 @@
 #' @param join.timeseries logical, for pure time-series plots, shall time series of multiple bands be plotted in a single plot (with different colors)?
 #' @param axes logical, if TRUE, plots include axes
 #' @param ... further arguments passed to \code{image.default}
-#' @note If caching is enabled for the package (see \code{\link{gdalcubes_cache}}), repeated calls of plot
+#' @note If caching is enabled for the package (see \code{\link{gdalcubes_use_cache}}), repeated calls of plot
 #' for the same data cube will not reevaluate the cube. Instead, the temporary result file will be reused, if possible.
 #' @note Some parts of the function have been copied from the stars package (c) Edzer Pebesma
 #' @export
